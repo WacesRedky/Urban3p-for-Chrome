34980f8c6bc4cd08ca708cb73c0291b290824daa
WacesRedky <wacesredky@gmail.com>
2012-10-13 00:36:41 +0400

    packed png's

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function badgeFormat(val) {
-	val = parseInt(val);
+	val = parseInt(val, 10);
 	if (val > 9999) val = Math.round(val/1000)+'k';
 	else if (val == 0) val = '';
 	else if (val < -999) val = Math.round(val/1000)+'k';
@@ -7,24 +7,43 @@ function badgeFormat(val) {
 	return val;
 }
 
-var updateChecker = function() {
-	$.get('http://urban3p.ru/api/user/', function(response){
-		var xml = $(response);
-		if (xml.find('error').length == 0) {
-			chrome.browserAction.setIcon({path:'img/icon_19.png'});
-			localStorage.isLogin = 1;
-			
-			localStorage.mailCount = parseInt(xml.find('mailCount').text());
-			
-			chrome.browserAction.setBadgeText({
-				text: badgeFormat(localStorage.mailCount)
-			});
-			chrome.browserAction.setBadgeBackgroundColor({
-				color: [44, 36, 27, 255]
-			});
-		} else {
-			localStorage.isLogin = 0;
-			chrome.browserAction.setIcon({path:'img/icon_19_ds.png'});
+var xhrSuccess = function(response) {
+	var data = JSON.parse(response);
+	if (!data.error) {
+		chrome.browserAction.setIcon({path:'img/icon_19.png'});
+		localStorage.isLogin = 1;
+		
+		localStorage.mailCount = parseInt(data.mailCount);
+		
+		chrome.browserAction.setBadgeText({
+			text: badgeFormat(localStorage.mailCount)
+		});
+		chrome.browserAction.setBadgeBackgroundColor({
+			color: [44, 36, 27, 255]
+		});
+	} else {
+		localStorage.isLogin = 0;
+		chrome.browserAction.setIcon({path:'img/icon_19_ds.png'});
+	}
+}
+
+var xhrProcess = function() {
+	try {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
+				xhrSuccess(this.responseText);
+			} else {
+				console.log("Не удалось получить данные:\n" + req.statusText);
+			}
 		}
-	});
+	} catch(e) {
+
+	}
+}
+
+var updateChecker = function() {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'http://urban3p.ru/api/user/?format=json', true);
+	xhr.onreadystatechange = xhrProcess;
+	xhr.send(null);
 }
